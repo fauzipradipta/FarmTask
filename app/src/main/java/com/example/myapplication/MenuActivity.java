b96d8e78bdcbc9a30a539966f4c3aa39d451cf94
fauzipradipta <dipta0988@gmail.com>
2022-10-11 18:21:25 -0700

    update

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -1,17 +1,59 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MenuActivity extends AppCompatActivity {
+    Intent cameraIntent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    button enableCamera = findViewById(R.id.enableCamera);
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        enableCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasCameraPermission()) {
+                    enableCamera();
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+
+
+    }
+    private boolean hasCameraPermission(){
+        return ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermission(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
+    private void enableCamera(){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
 
     }
 }

73ebbc15e59030ee9296df6c3c4b24e35734eade
fauzipradipta <dipta0988@gmail.com>
2022-11-07 18:10:48 -0800

    add database

--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -1,19 +1,68 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.sqlconnection.ConnectionClass;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static android.text.TextUtils.isEmpty;
+
 public class SignupActivity extends AppCompatActivity {
 
-    EditText editFirstName;
-    EditText editLastname;
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText password;
+
+    LinearLayout lvparent;
+
+    Button rButton; //Register Button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firstName = (EditText) findViewById(R.id.editFirstName);
+        lastName = (EditText) findViewById(R.id.editLastName);
+        email = (EditText) findViewById(R.id.Email);
+        password =(EditText) findViewById(R.id.Password);
+        rButton = (Button) findViewById(R.id.registerButton);
+
+        rButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isEmpty(firstName.getText().toString())||
+                isEmpty(lastName.getText().toString()) || isEmpty(email.getText().toString())
+                || isEmpty(password.getText().toString()))
+                    ShowSnackBar("Please Enter all fields");
+            }
+        });
     }
+    public void ShowSnackBar(String message) {
+        Snackbar.make(lvparent, message, Snackbar.LENGTH_LONG)
+                .setAction("CLOSE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light))
+                .show();
+    }
+
 }
--- app/src/main/java/com/example/sqlconnection/ConnectionClass.java
+++ app/src/main/java/com/example/sqlconnection/ConnectionClass.java
@@ -1,12 +1,43 @@
 package com.example.sqlconnection;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionClass {
-    public static String ip="";
-    public static String un ="";
-    public static String password="";
-    public static String db="Greenfield";
 
+    @SuppressLint("NewApi")
+    public static Connection CONN(){
+         String url="127.0.0.1:1433";
+         String username ="root";
+         String password="Temp123";
+         String db="Greenfield";
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+
+        try{
+            ConnURL = "jdbc:jtds:sqlserver://" + url + ";"
+                    + "databaseName=" + db + ";user=" + username + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+        Log.e("ERROR", se.getMessage());
+    } catch (ClassNotFoundException e) {
+        Log.e("ERROR", e.getMessage());
+    } catch (Exception e) {
+        Log.e("ERROR", e.getMessage());
+    }
+        return conn;
+
+    }
 
 
 }
